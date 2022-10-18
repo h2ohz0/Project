4eceffa5a6218310578f1f13d5428bd21c339e97
h2ohz0 <hjy04236@naver.com>
2022-10-18 22:53:40 +0900

    Initial commit

---
+++ 05.py
@@ -0,0 +1,7 @@
+from pico2d import *
+open_canvas()
+character = load_image('char.png')
+character.draw(100,100)
+character.draw(200,200)
+
+update_canvas()
---
+++ KillingBug.py
@@ -0,0 +1,67 @@
+from pico2d import *
+
+open_canvas()
+
+bg_jungle = load_image('background_jungle.png')
+character = load_image('char1.png')
+enemy = load_image('enemy1_1.png')
+
+
+def handle_events():
+    global running
+    global dir_x, dir_y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_d:
+                dir_x += 1
+            elif event.key == SDLK_a:
+                dir_x -= 1
+            elif event.key == SDLK_w:
+                dir_y += 1
+            elif event.key == SDLK_s:
+                dir_y -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_d:
+                dir_x -= 1
+            elif event.key == SDLK_a:
+                dir_x += 1
+            elif event.key == SDLK_w:
+                dir_y -= 1
+            elif event.key == SDLK_s:
+                dir_y += 1
+    pass
+
+
+running = True
+x = 200
+y = 300
+frame = 0
+dir_x = 0
+dir_y = 0
+while running:
+    clear_canvas()
+    bg_jungle.draw(400, 300)
+    character.draw(x, y)
+    enemy.clip_draw(frame * 100, 100 * 1, 103, 120, 600, 300)
+    x += dir_x * 5
+    y += dir_y * 5
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 5
+
+    delay(0.03)
+    if x > 400:
+        dir_x -= 1
+    elif x < 0:
+        dir_x += 1
+    elif y < 0:
+        dir_y += 1
+    elif y > 600:
+        dir_y -= 1
+
+close_canvas()
