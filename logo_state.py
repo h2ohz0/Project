a1187457bda5429098f37eb68b0062a1a8a966bd
h2ohz0 <hjy04236@naver.com>
2022-11-12 06:24:36 +0900

    연습

--- logo_state.py
+++ logo_state.py
@@ -9,19 +9,23 @@ logo_time = 0.0
 def enter():
     global image
     image = load_image('tuk_credit.png')
+    pass
 
 def exit():
     global image
     del image
+    pass
 
 def update():
     global logo_time
     # global running
     if logo_time > 1.0:
         logo_time = 0
+        # running = False
         game_framework.change_state(title_state)
-    delay(0.05)
-    logo_time += 0.05
+        # game_framework.quit()
+    delay(0.01)
+    logo_time += 0.01
 
 def draw():
     clear_canvas()
