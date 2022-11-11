fc4fc72292f1c38427f1f06c908ec6dfaf7b320c
h2ohz0 <hjy04236@naver.com>
2022-11-11 23:56:19 +0900

    연습

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -12,7 +12,25 @@ def handle_events():
             running = False
 
 # initialization code
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
 
+    def draw(self):
+        self.image.draw(400,30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0,90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100,0,100,100,self.x, self.y)
 # game main loop code
 
 # finalization code
