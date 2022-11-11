c348819626b07a9f6ec93ae661b56ea881f91db1
h2ohz0 <hjy04236@naver.com>
2022-11-12 06:24:50 +0900

    mygame 수정

--- KillingBug.py
+++ KillingBug.py
@@ -1,59 +1,70 @@
-from pico2d import *
+import game_framework
+import pico2d
 
-open_canvas()
+import play_state
 
-bg_jungle = load_image('background_jungle.png')
-character = load_image('char1.png')
-enemy = load_image('enemy1_1.png')
+pico2d.open_canvas(1600, 600)
+game_framework.run(play_state)
+pico2d.close_canvas()
 
-
-def handle_events():
-    global running
-    global dir_x, dir_y
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_d:
-                dir_x += 1
-            elif event.key == SDLK_a:
-                dir_x -= 1
-            elif event.key == SDLK_w:
-                dir_y += 1
-            elif event.key == SDLK_s:
-                dir_y -= 1
-            elif event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_d:
-                dir_x -= 1
-            elif event.key == SDLK_a:
-                dir_x += 1
-            elif event.key == SDLK_w:
-                dir_y -= 1
-            elif event.key == SDLK_s:
-                dir_y += 1
-    pass
-
-
-running = True
-x = 200
-y = 300
-frame = 0
-dir_x = 0
-dir_y = 0
-while running:
-    clear_canvas()
-    bg_jungle.draw(400, 300)
-    character.draw(x, y)
-    enemy.clip_draw(frame * 100, 100 * 1, 103, 120, 600, 300)
-    x += dir_x * 5
-    y += dir_y * 5
-    update_canvas()
-    handle_events()
-    frame = (frame + 1) % 5
-
-    delay(0.02)
-
-close_canvas()
+# import game_framework
+#
+# import play_state
+#
+# open_canvas()
+#
+# bg_jungle = load_image('background_jungle.png')
+# character = load_image('char1.png')
+# enemy = load_image('enemy1_1.png')
+#
+#
+# def handle_events():
+#     global running
+#     global dir_x, dir_y
+#     events = get_events()
+#     for event in events:
+#         if event.type == SDL_QUIT:
+#             running = False
+#         elif event.type == SDL_KEYDOWN:
+#             if event.key == SDLK_d:
+#                 dir_x += 1
+#             elif event.key == SDLK_a:
+#                 dir_x -= 1
+#             elif event.key == SDLK_w:
+#                 dir_y += 1
+#             elif event.key == SDLK_s:
+#                 dir_y -= 1
+#             elif event.key == SDLK_ESCAPE:
+#                 running = False
+#         elif event.type == SDL_KEYUP:
+#             if event.key == SDLK_d:
+#                 dir_x -= 1
+#             elif event.key == SDLK_a:
+#                 dir_x += 1
+#             elif event.key == SDLK_w:
+#                 dir_y -= 1
+#             elif event.key == SDLK_s:
+#                 dir_y += 1
+#     pass
+#
+#
+# running = True
+# x = 200
+# y = 300
+# frame = 0
+# dir_x = 0
+# dir_y = 0
+# while running:
+#     clear_canvas()
+#     bg_jungle.draw(400, 300)
+#     character.draw(x, y)
+#     enemy.clip_draw(frame * 100, 100 * 1, 103, 120, 600, 300)
+#     x += dir_x * 5
+#     y += dir_y * 5
+#     update_canvas()
+#     handle_events()
+#     frame = (frame + 1) % 5
+#
+#     delay(0.02)
+#
+# close_canvas()
