beb2283ab1cd8102c28b06fea95b733494693dab
h2ohz0 <hjy04236@naver.com>
2022-11-12 19:52:36 +0900

    타이틀 스테이트 편집, 게임월드 버그 수정 시도 -> 실패

--- title_state.py
+++ title_state.py
@@ -6,7 +6,7 @@ image = None
 
 def enter():
     global image
-    image = load_image('title.png')
+    image = load_image('Insector_X_Title.png')
 
 def exit():
     global image
@@ -43,35 +43,6 @@ def pause():
 
 def resume():
     pass
-
-class Boy:
-    def __init__(self):
-        self.x, self.y = 0, 90
-        self.frame = 0
-        self.image = load_image('run_animation.png')
-        self.ball_image = load_image('ball21x21.png')
-        self.big_ball_image = load_image('ball41x41.png')
-        self.item = None
-    def update(self):
-        self.frame = (self.frame + 1) % 8
-        self.x += 1
-    def draw(self):
-        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
-        if self.item == 'BigBall':
-            self.big_ball_image.draw(self.x+10, self.y+50)
-        elif self.item == 'Ball':
-            self.ball_image.draw(self.x+10, self.y+50)
-
-    def handle_events():
-        events = get_events()
-        for event in events:
-            if event.type == SDL_QUIT:
-                game_framework.quit()
-            elif event.type == SDL_KEYDOWN:
-                if event.key == SDLK_ESCAPE:
-                    game_framework.change_state(title_state)
-                elif event.key == SDLK_i:
-                    game_framework.push_state(item_state)
 
 
 def test_self():
