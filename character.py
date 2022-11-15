0ac44eba5d8a3c4aad9b64a8f143b10dfeb6da32
h2ohz0 <hjy04236@naver.com>
2022-11-15 23:33:25 +0900

    sleep 상태 수정

--- character.py
+++ character.py
@@ -85,12 +85,7 @@ class SLEEP:
         self.frame = (self.frame + 1) % 8
 
     def draw(self):
-        if self.face_dir == -1:
-            self.image.clip_composite_draw(self.frame * 100, 200, 100, 100,
-                                          -3.141592 / 2, '', self.x + 25, self.y - 25, 100, 100)
-        else:
-            self.image.clip_composite_draw(self.frame * 100, 300, 100, 100,
-                                          3.141592 / 2, '', self.x - 25, self.y - 25, 100, 100)
+            self.image.draw(200,300)
 
 
 #3. 상태 변환 구현
