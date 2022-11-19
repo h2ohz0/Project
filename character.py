def220a221352022f67886c38472058ab6207fc5
h2ohz0 <hjy04236@naver.com>
2022-11-19 19:58:58 +0900

    플레이어 캐릭터 스피드 올림

--- character.py
+++ character.py
@@ -7,7 +7,7 @@ width, height = 1024, 684
 blind = True
 
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
-RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_KMPH = 30.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
