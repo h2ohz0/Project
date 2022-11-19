21c3ab39ac553ae58608322acf1d0737d53e85c7
h2ohz0 <hjy04236@naver.com>
2022-11-19 21:51:06 +0900

    적 코드 수정

--- enemy.py
+++ enemy.py
@@ -1,53 +1,71 @@
-from pico2d import *
+from pico2d import*
+import mygame
+import random
+class Walk:
+    @staticmethod
+    def enter(obj):
+        obj.x = 900
+        obj.y = random.randint(150,550)
+        obj.frame = 0
+        obj.active = True
+    @staticmethod
+    def draw(obj):
+        obj.images[0].clip_draw(int(obj.frame) * 124, 0, 124, 73, obj.x, obj.y)
+    @staticmethod
+    def update(obj):
+        obj.frame = (obj.frame + 3 * mygame.ACTION_PER_TIME * mygame.frame_time) % 3
+    @staticmethod
+    def exit(obj):
+        pass
+    @staticmethod
+    def get_bb(obj):
+        return obj.x - 62, obj.y - 36.5, obj.x + 62, obj.y + 36.5
+class Dead:
+    @staticmethod
+    def enter(obj):
+        obj.frame = 0
+
+    @staticmethod
+    def draw(obj):
+
+        obj.images[1].clip_draw(int(obj.frame) * 174,0,174,175,obj.x,obj.y)
+
+    @staticmethod
+    def update(obj):
+        if obj.frame >= 5:
+            obj.active = False
+        obj.frame = (obj.frame + 6 * mygame.ACTION_PER_TIME * mygame.frame_time ) % 6
+    @staticmethod
+    def exit(obj):
+        obj.active = False
+    @staticmethod
+    def get_bb(obj):
+        return 0,0,0,0
+
 
 class Enemy:
     def __init__(self):
-        self.image = load_image('enemy1_1.png')
-
-    def update(self):
-        pass
-
-    def draw(self):
+        self.x = 900
+        self.y = 200
         self.frame = 0
-        self.image.clip_draw(self.frame * 100, 100 * 1, 103, 120, 600, 300)
-
-
-    def handle_events():
-        global running
-        global dir_x, dir_y
-        events = get_events()
-        for event in events:
-            if event.type == SDL_QUIT:
-                running = False
-            elif event.type == SDL_KEYDOWN:
-                if event.key == SDLK_d:
-                    dir_x += 1
-                elif event.key == SDLK_a:
-                    dir_x -= 1
-                elif event.key == SDLK_w:
-                    dir_y += 1
-                elif event.key == SDLK_s:
-                    dir_y -= 1
-                elif event.key == SDLK_ESCAPE:
-                    running = False
-            elif event.type == SDL_KEYUP:
-                if event.key == SDLK_d:
-                    dir_x -= 1
-                elif event.key == SDLK_a:
-                    dir_x += 1
-                elif event.key == SDLK_w:
-                    dir_y -= 1
-                elif event.key == SDLK_s:
-                    dir_y += 1
-        pass
-
-
-running = True
-
-while running:
-    clear_canvas()
-    update_canvas()
-    Enemy.handle_events()
-    frame = (frame + 1) % 5
-
-    delay(0.05)
+        self.images = [load_image("enemy1_2.png"),load_image("enemy1_3.png")]
+        # self.velocity = mygame.GRASS_SPEED_PPS * 1.5
+        self.active = False
+        self.current_state = Dead
+    def enter(self):
+        self.x = 900
+        self.current_state.enter(self)
+    def draw(self):
+        if self.active == True:
+            self.current_state.draw(self)
+    def update(self):
+        self.current_state.update(self)
+        mygame.move_update(self)
+    def exit(self):
+        self.current_state.exit(self)
+    def get_bb(self):
+        return self.current_state.get_bb(self)
+    def change_state(self,state):
+        self.current_state.exit(self)
+        self.current_state = state
+        self.current_state.enter(self)
